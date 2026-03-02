909f32b12aea4eb1e5584f65d591868192c70a04
pauloalarico <paulocraft20@gmail.com>
2026-03-01 23:48:58 -0300

    feat: implement the listener configuration for the process of order

--- src/main/java/org/order/payments/infra/kafka/configuration/PaymentConsumerConfiguration.java
+++ src/main/java/org/order/payments/infra/kafka/configuration/PaymentConsumerConfiguration.java
@@ -1,4 +1,53 @@
 package org.order.payments.infra.kafka.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
 public class PaymentConsumerConfiguration {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String server;
+
+    @Value("${apps.kafka.mapping}")
+    private String mapping;
+
+    @Value("${apps.kafka.trust-package}")
+    private String trustPackage;
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> configs = new HashMap<>(kafkaProperties.buildConsumerProperties());
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        configs.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, trustPackage);
+        configs.put(JacksonJsonDeserializer.TYPE_MAPPINGS, mapping);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> listenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.setConcurrency(3);
+        return factory;
+    }
 }
