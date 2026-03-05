9df3e0dd28ceedba453abecb7509d0a4ff45aa41
pauloalarico <paulocraft20@gmail.com>
2026-03-05 00:18:00 -0300

    refactor: inline the return of the mapper

--- src/main/java/org/order/payments/infra/mapper/PaymentCreateResponseMapper.java
+++ src/main/java/org/order/payments/infra/mapper/PaymentCreateResponseMapper.java
@@ -1,34 +1,21 @@
 package org.order.payments.infra.mapper;
 
 import org.order.payments.application.dto.response.PaymentCreatedResponse;
-import org.order.payments.domain.enums.EventPayment;
-import org.order.payments.domain.enums.MethodPayment;
 import org.order.payments.domain.model.Payment;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
-import java.util.UUID;
 
 @Component
 public class PaymentCreateResponseMapper {
     public PaymentCreatedResponse toDto(Payment payment) {
-        EventPayment eventPayment = payment.getEvent();
-        UUID paymentId = payment.getPaymentId();
-        UUID correlationId = payment.getCorrelationId();
-        UUID customerId = payment.getCustomer();
-        BigDecimal amount = payment.getAmount();
-        MethodPayment method = payment.getMethod();
-        ZonedDateTime createdAt = payment.getCreatedAt();
 
         return new PaymentCreatedResponse(
-                eventPayment,
-                paymentId,
-                correlationId,
-                customerId,
-                amount,
-                method,
-                createdAt
+                payment.getEvent(),
+                payment.getPaymentId(),
+                payment.getCorrelationId(),
+                payment.getCustomer(),
+                payment.getAmount(),
+                payment.getMethod(),
+                payment.getCreatedAt()
         );
     }
 }
