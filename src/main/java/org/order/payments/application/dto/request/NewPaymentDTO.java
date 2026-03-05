44b4dfbb7ca82528db4a6c51fe11738c39f3da34
pauloalarico <paulocraft20@gmail.com>
2026-03-05 00:02:56 -0300

    feature: add regex validation in the request dto, see if is really a uuid

--- src/main/java/org/order/payments/application/dto/request/NewPaymentDTO.java
+++ src/main/java/org/order/payments/application/dto/request/NewPaymentDTO.java
@@ -2,6 +2,7 @@ package org.order.payments.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import org.order.payments.domain.enums.MethodPayment;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 
 public record NewPaymentDTO(
         @NotBlank
+        @Pattern(regexp = "^[a-zA-Z0-9\\-]{1,64}$", message = "Invalid format of id")
         String consumerId,
         @Positive
         BigDecimal amount,
